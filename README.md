c8f8b1adefdfb1240f502cd57fe5f9a42653a401
Naveen Malik <nmalik@redhat.com>
2021-11-28 11:42:16 -0500

    Initial code etc

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # timeOfDay-exporter
 Simple exporter for the time of day. Useful to determine when it's day vs night for astrophotography.
+
+# Setup
+
+Make sure you have required software:
+* python
+* pip
+* git
+
+## Clone Source
+
+I am running on ec2 and simply clone into the ec2-user's home directory.
+
+```shell
+git clone https://github.com/jewzaam/timeOfDay-exporter
+```
+
+## config.yaml
+
+You can name the file whatever you want, but this document assumes **config.yaml**.
+
+```yaml
+location:
+  latitude: <your latitude in decimal form>
+  longitude: <your longitude in decimal form>
+refresh_delay_seconds: 3600 # don't need often, once an hour
+```
+s
+## Python requirements
+
+```shell
+pip install -r requirements.txt --user
+```
+
+## Linux Service
+
+Install the service by setting up some env vars then copying the systemd template with those vars, start the service, and enable the service.
+
+```shell
+export REPO_BASE_DIR=~/timeOfDay-exporter
+export PORT=8012
+export CONFIG=$REPO_BASE_DIR/config.yaml
+export PYTHON=$(which python)
+
+cat $REPO_BASE_DIR/src/systemd/timeOfDay-exporter.service | envsubst > /tmp/timeOfDay-exporter.service
+sudo mv /tmp/timeOfDay-exporter.service /etc/systemd/system/timeOfDay-exporter.service
+
+unset REPO_BASE_DIR
+unset PORT
+unset CONFIG
+unset PYTHON
+
+sudo systemctl daemon-reload
+sudo systemctl start timeOfDay-exporter.service
+sudo systemctl enable timeOfDay-exporter.service
+```
+
+# Verify
+
+## Metrics 
+Check the metrics are exported on the port you specified.
---
+++ src/py/timeOfDay-exporter.py
@@ -0,0 +1,96 @@
+import argparse
+from types import LambdaType
+import time
+import requests
+import json
+import yaml
+import traceback
+import datetime
+
+import httpimport
+
+with httpimport.github_repo('jewzaam', 'metrics-utility', 'utility', 'main'):
+    import utility
+
+DEBUG=True
+
+def debug(message):
+    if DEBUG:
+        print("DEBUG: {}".format(message))
+
+def do_timeOfDay(config):
+    lat = config['location']['latitude']
+    long = config['location']['longitude']
+
+    # create labels common to all metrics
+    global_labels={
+        "latitude": lat,
+        "longitude": long,
+    }
+
+    while True:
+        # https://api.sunrise-sunset.org/json?lat=35.605350&lng=-78.793549&formatted=0
+        response = requests.get("https://api.sunrise-sunset.org/json?lat={}&lng={}&formatted=0".format(lat,long))
+
+        if response.status_code != 200 or response.text is None or response.text == '':
+            utility.inc("timeOfDay_error_total")
+        else:
+            data = json.loads(response.text)
+
+            try:
+                begin_day=0
+                begin_night=0
+                if 'results' in data:
+                    for key in data['results']:
+                        value = data['results'][key]
+                        if not isinstance(value, str):
+                            continue
+                        # convert UTC string date to seconds since epoch
+                        # example: 2021-08-13T01:42:21+00:00
+                        utc_time = datetime.datetime.strptime(value.split("+")[0], "%Y-%m-%dT%H:%M:%S")
+                        epoch_time = (utc_time - datetime.datetime(1970, 1, 1)).total_seconds()
+
+                        utility.set("timeOfDay_{}".format(key), epoch_time, global_labels)
+
+                        if key == 'astronomical_twilight_end':
+                            begin_night = epoch_time
+                        elif key == 'astronomical_twilight_begin':
+                            begin_day = epoch_time
+
+                # easier to fabricate is_day and is_night in python
+                now=time.time()
+                if begin_day<now:
+                    begin_day += 24*60*60
+                elif begin_day>now and begin_night>now:
+                    begin_night -= 24*60*60
+                utility.set("timeOfDay_begin_day",begin_day,global_labels)
+                utility.set("timeOfDay_begin_night",begin_night,global_labels)
+
+                utility.inc("timeOfDay_success_total", global_labels)
+            except Exception as e:
+                # well something went bad
+                print(repr(e))
+                traceback.print_exc()
+                utility.inc("timeOfDay_error_total", global_labels)
+
+        # sleep for the configured time
+        time.sleep(config['refresh_delay_seconds'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Export logs as prometheus metrics.")
+    parser.add_argument("--port", type=int, help="port to expose metrics on")
+    parser.add_argument("--config", type=str, help="configuraiton file")
+    
+    args = parser.parse_args()
+    
+    # Start up the server to expose the metrics.
+    utility.metrics(args.port)
+
+    config = {}
+    with open(args.config, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    do_timeOfDay(config)
+
+# python exporter-timeOfDay.py --port 8001 --config exporter-timeOfDay.yaml
